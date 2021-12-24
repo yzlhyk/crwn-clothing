a2c427e1fed3af0782b67a5f4355ae10cd797b44
Lincoln <lincolny@ngis.com.au>
2021-12-24 16:45:55 +0800

    80. withRoutes
    - actually did not use that withRoutes,  the code implement as react router v6 version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,83 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route, Link, useParams, useNavigate } from 'react-router-dom';
+
+// const HomePage = () => {
+//   let navigate = useNavigate();
+//   const handleClick = () => {
+//     navigate("/topics");
+//   }
+//   console.log(navigate)
+//   return (
+//     <div>
+//       <button onClick={handleClick}>TOPICS</button>
+//       <h1>HOME PAGE</h1>
+//     </div>
+//   );
+// };
+
+// const TopicsList = () => {
+//   return (
+//     <div>
+//       <nav>
+//         <Link to='me'>Static component Me</Link> <br />
+//         <Link to='12'>to topic 12</Link> <br />
+//         <Link to='34'>to topic 34</Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path=':topicId' element={<TopicDetail />} />
+//         <Route path='me' element={<OwnUserProf />} />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// const TopicDetail = () => {
+//   let { topicId } = useParams();
+//   console.log(topicId);
+//   return (
+//     <div>
+//       <h1>TOPIC DETAIL PAGE: {topicId}</h1>
+//     </div>
+//   );
+// };
+
+// const OwnUserProf = () => {
+//   return (
+//     <div>
+//       <h1>User profile me</h1>
+//     </div>
+//   )
+// }
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
+
+const JacketsPage = () => {
+  return (
+    <div>
+      <h1>JACKETS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+        <Route path='/jackets' element={<JacketsPage />} />
+        {/* <Route path='/topics/*' element={<TopicsList />} /> */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { useParams, useNavigate, useMatch, useLocation } from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, }) => {
+
+  let { pathname } = useLocation();
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`${pathname}${linkUrl}`);
+  }
+
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={handleClick}>
       <div
         className='background-image'
         style={{
@@ -11,11 +20,12 @@ const MenuItem = ({ title, imageUrl, size }) => {
         }}
       />
       <div className="content">
-        <div className="title">{title.toUpperCase()}</div>
+        <div className="title">{title}</div>
         <div className="subtitle">SHOP NOW</div>
       </div>
     </div>
   )
 };
 
+// export default withRouter(MenuItem);
 export default MenuItem;
