5e54ca8e5ea8ad72394fa13246fcd2c2dc16d530
Lincoln <lincolny@ngis.com.au>
2022-01-04 20:40:06 +0800

    90 adding Sign In component page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Routes, Route} from 'react-router-dom';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
 
 // const HomePage = () => {
@@ -60,6 +61,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
         {/* <Route path='/topics/*' element={<TopicsList />} /> */}
       </Routes>
     </div>
